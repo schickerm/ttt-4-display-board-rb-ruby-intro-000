b37f64bb31ab0ae0bd4f461460ce1ced2fc61691
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-24 21:21:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -48,3 +48,15 @@ def display_board(board)
 end
 
 display_board(board)
+
+ard = ["O", "  ", "  ", "  ", "X", "  ", "  ", "  ", "  "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------" 
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
