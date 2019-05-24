0c353886310dbaa5335df899930db649cb9d2b58
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-24 21:17:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,7 +13,26 @@ end
  
 display_board(board)
 
+board = ["  ", "  ", "  ", "  ", "X", "  ", "  ", "  ", "  "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------" 
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+ 
 display_board(board[4] = "X")
 
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------" 
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 display_board(board[0] = "O")
 
